a2f98ceb816a3b4f9394532e124e1c3d172ce84a
PJMonticoli <pablo_monticoli@hotmail.com>
2024-07-05 16:39:14 -0300

    Modifico color de fuente para mejorar visibilidad light mode y dark mode

--- pablodev/tailwind.config.mjs
+++ pablodev/tailwind.config.mjs
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		function({ addUtilities }) {
-		  addUtilities({
-			'.notranslate': {
-			  'translate': 'no',
-			},
-		  })
-		},
-	  ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        lightText: "#2d2d2d",
+        darkText: "#e5e5e5",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".notranslate": {
+          translate: "no",
+        },
+      });
+    },
+  ],
+};
